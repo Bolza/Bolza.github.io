a652dd55e904b92220fda12f50ee3ccf57ec70ff
Stefano Sergio <stefano@fathomlondon.com>
2017-08-03 13:38:21 +0100

    versioning

--- email.js
+++ email.js
@@ -1,14 +1,17 @@
 // var emailElement = document.querySelector("email");
+function getEmail() {
+    var parts = ['ilbolza', 'gmail', 'com'];
+    return parts[0] + '@' + parts[1] + '.' + parts[2];
+}
+
 function mailTo() {
-    var email = this.attributes['data-user'].value + '@' + this.attributes['data-domain'].value;
-    location.href='mailto:' + email;
+    location.href='mailto:' + getEmail();
 }
 
 function copyEmail() {
     var textArea = document.createElement("textarea");
-    var text = 'ilbolza';
     document.body.appendChild(textArea);
-    textArea.value = text;
+    textArea.value = getEmail();
     textArea.select();
     document.execCommand('copy');
     document.body.removeChild(textArea);
